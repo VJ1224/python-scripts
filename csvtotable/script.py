b12659393eefe1667b19e78cdbe9f5e5f0990612
VJ1224 <vanshjain1224@gmail.com>
2020-08-08 13:48:22 +0530

    Switched to main:

--- csvtotable/script.py
+++ csvtotable/script.py
@@ -3,41 +3,45 @@ import sys
 import csv
 import os
 
-if len(sys.argv) == 1:
-    print("Provide filename as command line argument")
-    sys.exit()
+def main():
+    if len(sys.argv) == 1:
+        print("Provide filename as command line argument")
+        return
 
-file = sys.argv[1]
+    file = sys.argv[1]
 
-if not os.path.isfile(file):
-    print("File does not exist")
-    sys.exit()
+    if not os.path.isfile(file):
+        print("File does not exist")
+        return
 
-if file[-3:] != "csv":
-    print("Not a .csv file")
-    sys.exit()
+    if file[-3:] != "csv":
+        print("Not a .csv file")
+        return
 
-with open(file, newline='', encoding='utf-8') as csvFile:
-    csv_reader = csv.reader(csvFile)
-    headings = next(csv_reader)
-    
-    database_name = file[:-3] + ".db"
-    connection = sqlite3.connect(database_name)
-    cursor = connection.cursor()
+    with open(file, newline='', encoding='utf-8') as csvFile:
+        csv_reader = csv.reader(csvFile)
+        headings = next(csv_reader)
+        
+        database_name = file[:-3] + ".db"
+        connection = sqlite3.connect(database_name)
+        cursor = connection.cursor()
 
-    query = "CREATE TABLE IF NOT EXISTS data ({column_names})".format(column_names=','.join(headings))
-    cursor.execute(query)
+        query = "CREATE TABLE IF NOT EXISTS data ({column_names})".format(column_names=','.join(headings))
+        cursor.execute(query)
 
-    records = []
-    for row in csv_reader:
-        records.append(tuple(row))
-    
-    vals = "("
-    for column in headings:
-        vals += "?,"
-    vals = vals[:-1] + ")"
+        records = []
+        for row in csv_reader:
+            records.append(tuple(row))
+        
+        vals = "("
+        for column in headings:
+            vals += "?,"
+        vals = vals[:-1] + ")"
 
-    cursor.executemany("INSERT INTO data VALUES {values}".format(values=vals), records)
+        cursor.executemany("INSERT INTO data VALUES {values}".format(values=vals), records)
 
-    connection.commit()
-    connection.close()
+        connection.commit()
+        connection.close()
+
+if __name__ == "__main__":
+    main()
